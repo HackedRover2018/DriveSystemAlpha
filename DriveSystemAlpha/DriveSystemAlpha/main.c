00e0e68595a5c94fb172adf7627325316bbf9dcb
Brad Ofrim <ofrim@ualberta.ca>
2018-01-27 12:43:25 -0800

    Create Atmel Studio project

---
+++ DriveSystemAlpha/DriveSystemAlpha/main.c
@@ -0,0 +1,18 @@
+/*
+ * DriveSystemAlpha.c
+ *
+ * Created: 1/27/2018 12:39:46 PM
+ * Author : Brad
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
