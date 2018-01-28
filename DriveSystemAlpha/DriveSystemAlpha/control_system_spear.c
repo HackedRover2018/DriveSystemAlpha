cb8feb000f46551717e952ac96a12ccfffc5bdad
Brad Ofrim <ofrim@ualberta.ca>
2018-01-27 19:28:33 -0800

    Add control functions

--- DriveSystemAlpha/DriveSystemAlpha/control_system_spear.c
+++ DriveSystemAlpha/DriveSystemAlpha/control_system_spear.c
@@ -1,6 +1,14 @@
 #include "control_system_spear.h"
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define DELTA_SCALE 0.5
+#define MAGNIUTUDE_SCALE 2/3
+
+// Private functions
+double left_set_point_delta(struct polar_coordinate in_polar);
+double right_set_point_delta(struct polar_coordinate in_polar);
 
 void command_to_polar(char* in_str, struct polar_coordinate* out_polar_coord)
 {
@@ -9,3 +17,23 @@ void command_to_polar(char* in_str, struct polar_coordinate* out_polar_coord)
     char* dir_str = strtok(NULL, " ");
     out_polar_coord->dir = atof(dir_str);
 }
+
+struct motor_levels compute_levels(struct polar_coordinate in_polar)
+{
+    struct motor_levels out_levels;
+    out_levels.right_level = right_set_point_delta(in_polar);
+    out_levels.left_level = left_set_point_delta(in_polar);
+    return out_levels;
+}
+
+double left_set_point_delta(struct polar_coordinate in_polar)
+{
+    return  MAGNIUTUDE_SCALE * (in_polar.mag * sin(in_polar.dir) + DELTA_SCALE * in_polar.mag * cos(in_polar.dir));
+}
+
+double right_set_point_delta(struct polar_coordinate in_polar)
+{
+    return MAGNIUTUDE_SCALE * (in_polar.mag * sin(in_polar.dir) - DELTA_SCALE * in_polar.mag * cos(in_polar.dir));
+}
+
+
--- DriveSystemAlpha/DriveSystemAlpha/control_system_spear.h
+++ DriveSystemAlpha/DriveSystemAlpha/control_system_spear.h
@@ -7,6 +7,16 @@ struct polar_coordinate
     double dir;
 };
 
+struct motor_levels
+{
+    double left_level;
+    double right_level;
+};
+
 void command_to_polar(char* in_str, struct polar_coordinate* out_polar_coord);
+
+struct motor_levels compute_levels(struct polar_coordinate in_polar);
+
+
 
 #endif /* CONTROL_SYSTEM_SPEAR_H_ */
